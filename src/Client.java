a7c7905f75d602bc0b7217e878f7a4c43642577a
PLatchenkov <baldr1992@gmail.com>
2018-03-27 17:39:02 +0300

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.Socket;
+
+
+public class Client {
+    public static void main(String args[]) throws InterruptedException {
+        try {
+            Socket socket = new Socket("127.0.0.1", 4242);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            while (!socket.isOutputShutdown()) {
+
+                if (bufferedReader.ready()) {
+                    System.out.println("Client starts to writing in channel");
+                    Thread.sleep(1000);
+                    String text = bufferedReader.readLine();
+
+                    outputStream.writeUTF(text);
+                    outputStream.flush();
+                    System.out.println("Your message " + text +"  has been sent");
+                    Thread.sleep(1000);
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Something went wrong");
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,47 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+
+    public static void main(String args[]) throws InterruptedException {
+        try {
+            ServerSocket serverSocket = new ServerSocket(4242);
+            Socket client = serverSocket.accept();
+            System.out.println("Connection was accepted");
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            System.out.println("DataInputStream created");
+            while (!client.isClosed()) {
+                System.out.println("System is ready for getting data");
+                String entry = in.readUTF();
+                System.out.println("Read message from Client: \n" + entry);
+                Thread.sleep(3000);
+                // инициализация проверки условия продолжения работы с клиентом по этому сокету по кодовому слову
+                //       - quit
+
+                if(entry.equalsIgnoreCase("quit")) {
+                    System.out.println("Ending of work");
+                    out.writeUTF("Server reply" + entry);
+                    out.flush();
+                    Thread.sleep(3000);
+                    break;
+                }
+                //out.writeUTF(entry);
+                //out.flush();
+                Thread.sleep(1000);
+            }
+            out.close();
+            in.close();
+            Thread.sleep(1000);
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Something went wrong");
+        }
+    }
+}
+
