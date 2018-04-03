72c1d1b47deaf956e33c48408bdfa81fff61e0ce
Paul <baldr92>
2018-04-04 00:02:17 +0300

    evening changes

--- src/ClientSender.java
+++ src/ClientSender.java
@@ -8,7 +8,7 @@ public class ClientSender {
             Socket socket = new Socket("127.0.0.1", 4242);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            //DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             while (!socket.isOutputShutdown()) {
 
         //впилил получение инфы 02.04.2018
@@ -18,9 +18,9 @@ public class ClientSender {
                     outputStream.writeUTF(text);
                     outputStream.flush();
                     System.out.println("Your message " + "<<" + text + ">>" + "  has been sent");
-                    System.out.println("Гет реди фо геттин сообщение с сервера");
-                    String getTextFromServer = inputStream.readUTF();
-                    System.out.println(getTextFromServer);
+                    //System.out.println("Гет реди фо геттин сообщение с сервера");
+                    //String getTextFromServer = inputStream.readUTF();
+                    //System.out.println(getTextFromServer);
                 }
             }
         } catch (IOException e) {
