64d7296d311ae5f7e5aae77127fee0b4e5a875c5
Paul <baldr92>
2018-04-23 23:36:44 +0300

    Недоделан датабус

--- src/Databus.java
+++ src/Databus.java
@@ -22,16 +22,42 @@ public class Databus {
 
                 while (!serverSocket.isClosed()) {
 
-                    Socket getFromClient = serverSocket.accept(); //посмотреть почему эти две
-                    String s; // проверка и отсечение
+                    Socket getFromClient = serverSocket.accept();
                     DataInputStream inputForParsing = new DataInputStream(getFromClient.getInputStream());
                     String parserText = inputForParsing.readUTF();
                     inputForParsing.close();
-                    if
-                    es.execute(new InputService(getFromClient));//строчки не выполняются в теле иф-контроллера
-                    //Socket sendToClient = serverSocket.accept();
-                    es.execute(new OutputService(sendToClient));//отсутствие гибкости программы, вследствии последовательного выполнения получения-отправки сообщения в очередь
-                }
+                        if (parserText.contains("-send")){
+                            Socket socketForSending = serverSocket.accept();
+                                try { //утащить в другой класс
+                                    DataInputStream inputStream = new DataInputStream(clientDialogue.getInputStream());
+                                    System.out.println("DataOutputStream is created");
+                                    System.out.println("DataInputStream is created");
+                                    while (!clientDialogue.isClosed()) {
+                                        //reading block
+                                        System.out.println("Start reading");
+                                        String message = inputStream.readUTF();
+
+                                        FileReader fileForCheck = new FileReader("Titles.txt");
+                                        BufferedReader bufferedReader = new BufferedReader(fileForCheck);
+                                        if (!(bufferedReader.readLine() == null)) {
+                                            System.out.println(message);
+                                            File file = new File("Titles.txt");
+                                            FileWriter fileWriter = new FileWriter(file, true);
+                                            fileWriter.write(message + "\n");
+                                            fileWriter.close();
+                                        }
+                                        inputStream.close();
+                                        //clientDialogue.close();
+                                    }
+                                } catch (IOException io) {
+                                    io.printStackTrace();
+                                    System.out.println(io.getMessage());
+                                }
+                        } else if(parserText.contains("-get")){
+                            //Socket sendToClient = serverSocket.accept();
+                            es.execute(new OutputService(sendToClient));//отсутствие гибкости программы, вследствии последовательного выполнения получения-отправки сообщения в очередь
+                        }
+                    }
             } catch (IOException e) {
                 e.printStackTrace();
                 System.out.println("Something went wrong");
