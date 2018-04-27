b8e2ab134c0ac916d3c798b3ecf70e69f2fc97bf
PLatchenkov <baldr1992@gmail.com>
2018-04-27 17:16:23 +0300

    Попытка реализовать чтение файла со стороны сервера и отправка данных клиенту. Работает только отправка клиентом.

--- src/Databus.java
+++ src/Databus.java
@@ -1,14 +1,19 @@
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 
 public class Databus {
     public static void main(String args[]) {
 
         ExecutorService es = Executors.newFixedThreadPool(6);
+        Databus databus = new Databus();
             try {
                 ServerSocket serverSocket = new ServerSocket(4242);
                 System.out.println("Connection was accepted");
@@ -50,8 +55,9 @@ public class Databus {
 
                         //es.execute(new InputService(getFromClient));
                     } else if (readCommandFromServer.contains("-get")){
-                        System.out.println("Поток на отправку создан");
-                        es.execute(new OutputService(getFromClient));
+                        //send message to client and remove last readed string
+                        databus.sendMessageToClient();
+
 
                     }
 
@@ -67,7 +73,42 @@ public class Databus {
             }
         //es.shutdown();
     }
+    public void sendMessageToClient() throws FileNotFoundException, IOException { //должно делаться на сервере
+        System.out.println("Конечный клиент подключился");
+        System.out.println("Подготовка к отправке сообщения клиенту");
+        File fileMsg = new File("Titles.txt");
+        //FileReader fileReader = new FileReader(fileMsg);
+        String lastLine = readLastLine(fileMsg);
+        //Socket socket = serverSocket.accept();
+        Socket newSocketToClient = new Socket("127.0.0.1", 4444);
+        DataOutputStream sendMsgStream = new DataOutputStream(newSocketToClient.getOutputStream());
+        sendMsgStream.writeUTF(lastLine);
+        System.out.println("Your message" + "<<" + lastLine + ">>" + "has been sent");
+        //на удаление последней строки
+        List<String> lines = FileUtils.readLines(fileMsg);
+        List<String> updatedLines = lines.stream().filter(s -> !s.contains(lastLine)).collect(Collectors.toList());
+        FileUtils.writeLines(fileMsg,updatedLines);
+    }
+
+    private static String readLastLine(File file) throws FileNotFoundException, IOException {
+        String result = null;
+        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+            long startIdx = file.length();
+            while (startIdx >= 0 && (result == null || result.length() == 0)) {
+                raf.seek(startIdx);
+                if (startIdx > 0)
+                    raf.readLine();
+                result = raf.readLine();
+                startIdx--;
+            }
+        }
+        return result;
+    }
 }
+
+
+
+
 
 
 
--- src/OneClient.java
+++ src/OneClient.java
@@ -1,79 +1,60 @@
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-import javafx.scene.SubScene;
-
-import javax.xml.stream.events.Characters;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 import org.apache.commons.io.FileUtils;
 
 public class OneClient {
-
-
-
-
-
     public static void main (String args[]) {
         //try {
+
             OneClient oneClient = new OneClient();
             System.out.println("Choose mode for the next work");
             System.out.println("Enter one of commands");
             System.out.println("\"-send\" or \"-get\" and your message");
 
-
-         //Scanner scanner = new Scanner(System.in);
-         //String switcherClientLogic = scanner.toString();
          BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
          try {
+             Socket socket = new Socket("127.0.0.1", 4242);
              String textFromPanel = bf.readLine();
              bf.close();
              if (textFromPanel.contains("-get")) {
+                 ServerSocket serverSocket = new ServerSocket(4444);
+                 Socket getFromDataBus = serverSocket.accept();
+                 DataInputStream getFromMQ = new DataInputStream(getFromDataBus.getInputStream());
+                 String stringMQ = getFromMQ.readUTF();
+                 System.out.println(stringMQ);
+                 /*
                     System.out.println("The system is prepared to get message");
-                    oneClient.getMessageFromQuery();
+                    //oneClient.getMessageFromQuery();
+                    oneClient.sendMessageToServer(textFromPanel, socket);
+                    System.out.println("1");
+                    DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                    inputStream.readUTF();
+                    */
                 } else if (textFromPanel.contains("-send ")) {
                     System.out.println("A message is prepared to send");
-                    oneClient.sendMessageToServer(textFromPanel);
+                    oneClient.sendMessageToServer(textFromPanel, socket);
                 } else {
                     System.out.println("Input one of following commands \"-get\" or \"send\" ");
                 }
             } catch (IOException io) {
                 io.printStackTrace();
             }
-                /*
-                switch (switcherClientLogic) {
-                    case switcherClientLogic.contains("-send"):
-                        System.out.println("Input your message");
-                        oneClient.sendMessageToServer();
-                        break;
-                    case "get":
-                        oneClient.getMessageFromServer();
-                        break;
-                    default:
-                        System.out.println("Choose option you would like to use");
-                        break;
-                }  */
-
     }
 
-
-    public void sendMessageToServer(String string) {
+    //сделать этот метод общим, а внутри сделать ветвление на отправку/прием
+    public void sendMessageToServer(String string, Socket socketToSever) {
         try{
-            Socket socketToSever = new Socket("127.0.0.1", 4242);
-            //BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             DataOutputStream outputStream = new DataOutputStream(socketToSever.getOutputStream());
             while (!socketToSever.isOutputShutdown()) {
-                //if (bf.ready()) {
                     System.out.println("ClientSender starts writing in channel");
-                    //String textToServer = bf.readLine();
                     outputStream.writeUTF(string);
                     outputStream.flush();
                     System.out.println("Your message " + "<<" + string + ">>" + "  has been sent");
                     System.out.println("Socket is closed");
                     socketToSever.shutdownOutput();
-                //}
             }
             //socketToSever.shutdownOutput();
             outputStream.close();
@@ -97,27 +78,17 @@ public class OneClient {
         }
     }
 
-    public void getMessageFromQuery() throws FileNotFoundException, IOException {
+    public void getMessageFromQuery() throws FileNotFoundException, IOException { //должно делаться на сервере
         System.out.println("Конечный клиент подключился");
         System.out.println("Подготовка к отправке сообщения клиенту");
         File fileMsg = new File("Titles.txt");
         FileReader fileReader = new FileReader(fileMsg);
-
-        //BufferedReader br = new BufferedReader(fileReader);
-        //String line = br.readLine();
-
-
-
-
-
         String lastLine = readLastLine(fileMsg);
         System.out.println(lastLine);
-
         //на удаление последней строки
         List<String> lines = FileUtils.readLines(fileMsg);
         List<String> updatedLines = lines.stream().filter(s -> !s.contains(lastLine)).collect(Collectors.toList());
         FileUtils.writeLines(fileMsg,updatedLines);
-
         }
 
         private static String readLastLine(File file) throws FileNotFoundException, IOException {
@@ -132,8 +103,6 @@ public class OneClient {
                 startIdx--;
             }
         }
-
-
         return result;
     }
 }
